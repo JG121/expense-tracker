10c54bc6df65641b1e7f1482711def200bd613aa
JG121 <125163352+JG121@users.noreply.github.com>
2023-07-09 18:56:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,114 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  $("#name").text(profile.getName());
+  $("#email").text(profile.getEmail());
+  $("#image").attr('src', profile.getImageUrl());
+  $(".data").css("display", "block");
+  $(".g-signin2").css("display", "none");
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    alert("You have been signed out successfully");
+    $(".g-signin2").css("display", "block");
+    $(".data").css("display", "none");
+  });
+}
+
+
+
+
 function getTotalExpenseAmount() {
-    let totalExpense = 0;
-    const expenseItems = document.querySelectorAll(".expense-list li:not([style*='none'])");
-  
-    for (const expenseItem of expenseItems) {
-      const expenseAmount = expenseItem.querySelector(".expense-amount").innerHTML;
-      const expenseAmountNumber = Number(expenseAmount);
-      totalExpense += expenseAmountNumber;
-    }
-  
-    return totalExpense;
+  let totalExpense = 0;
+  const expenseItems = document.querySelectorAll(".expense-list li:not([style*='none'])");
+
+  for (const expenseItem of expenseItems) {
+    const expenseAmount = expenseItem.querySelector(".expense-amount").innerHTML;
+    const expenseAmountNumber = Number(expenseAmount);
+    totalExpense += expenseAmountNumber;
   }
-  
-  // Update the total expense amount in the DOM
-  function updateTotalExpenseAmount() {
-    const totalExpense = getTotalExpenseAmount();
-    const totalAmountElement = document.querySelector("#total-amount");
-    const currencySelectElement = document.querySelector("#currency-select");
-    const currency = currencySelectElement.value;
-    totalAmountElement.innerHTML = `${totalExpense} ${currency}`;
-  }
-  
-  // Event listener for the submit button
-  document.querySelector(".expense-form").addEventListener("submit", (event) => {
-    event.preventDefault();
-  
-    // Get the expense amount
-    const expenseAmount = document.querySelector("#expense-input").value;
-    const expenseCategory = document.querySelector("#expense-category").value;
-    const expenseDate = document.querySelector("#expense-date").value;
-  
-    // Create a new expense item
-    const expenseItem = document.createElement("li");
-    expenseItem.innerHTML = `
-      <span class="expense-category">${expenseCategory}</span>
-      <span class="expense-amount">${expenseAmount}</span>
-      <span class="expense-date">${expenseDate}</span>
-    `;
-  
-    // Add the expense item to the expense list
-    const expenseList = document.querySelector(".expense-list");
-    expenseList.appendChild(expenseItem);
-  
-    // Update the total expense amount
-    updateTotalExpenseAmount();
-  });
-  
-  // Event listener for the increase button
-  document.querySelector("#increase-button").addEventListener("click", () => {
-    let expenseAmount = document.querySelector("#expense-input").value;
-    expenseAmount++;
-    document.querySelector("#expense-input").value = expenseAmount;
-  });
-  
-  // Event listener for the decrease button
-  document.querySelector("#decrease-button").addEventListener("click", () => {
-    let expenseAmount = document.querySelector("#expense-input").value;
-    expenseAmount--;
-    document.querySelector("#expense-input").value = expenseAmount;
-  });
-  
-  // Function to filter expenses based on month and year
-  function filterExpenses() {
-    const filterMonth = document.querySelector("#filter-month").value.toLowerCase();
-    const filterYear = document.querySelector("#filter-year").value.toLowerCase();
-    const expenseItems = document.querySelectorAll(".expense-list li");
-  
-    for (const expenseItem of expenseItems) {
-      const expenseDate = expenseItem.querySelector(".expense-date").innerHTML.toLowerCase();
-      if (filterMonth !== "all" && filterYear !== "all") {
-        if (expenseDate.includes(filterMonth) && expenseDate.includes(filterYear)) {
-          expenseItem.style.display = "block";
-        } else {
-          expenseItem.style.display = "none";
-        }
-      } else {
-        expenseItem.style.display = "block";
-      }
-    }
-  
-    updateTotalExpenseAmount();
-  }
-  
-  // Event listener for the filter form
-  document.querySelector(".filter-form").addEventListener("submit", (event) => {
-    event.preventDefault();
-    filterExpenses();
-  });
-  
-  // Initialize the script
+
+  return totalExpense;
+}
+
+// Update the total expense amount in the DOM
+function updateTotalExpenseAmount() {
+  const totalExpense = getTotalExpenseAmount();
+  const totalAmountElement = document.querySelector("#total-amount");
+  const currencySelectElement = document.querySelector("#currency-select");
+  const currency = currencySelectElement.value;
+  totalAmountElement.innerHTML = `${totalExpense} ${currency}`;
+}
+
+// Event listener for the submit button
+document.querySelector(".expense-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Get the expense amount
+  const expenseAmount = document.querySelector("#expense-input").value;
+  const expenseCategory = document.querySelector("#expense-category").value;
+  const expenseDate = document.querySelector("#expense-date").value;
+
+  // Create a new expense item
+  const expenseItem = document.createElement("li");
+  expenseItem.innerHTML = `
+    <span class="expense-category">${expenseCategory}</span>
+    <span class="expense-amount">${expenseAmount}</span>
+    <span class="expense-date">${expenseDate}</span>
+  `;
+
+  // Add the expense item to the expense list
+  const expenseList = document.querySelector(".expense-list");
+  expenseList.appendChild(expenseItem);
+
+  // Update the total expense amount
   updateTotalExpenseAmount();
+});
+
+// Event listener for the increase button
+document.querySelector("#increase-button").addEventListener("click", () => {
+  let expenseAmount = document.querySelector("#expense-input").value;
+  expenseAmount++;
+  document.querySelector("#expense-input").value = expenseAmount;
+});
+
+// Event listener for the decrease button
+document.querySelector("#decrease-button").addEventListener("click", () => {
+  let expenseAmount = document.querySelector("#expense-input").value;
+  expenseAmount--;
+  document.querySelector("#expense-input").value = expenseAmount;
+});
+
+// Function to filter expenses based on month and year
+function filterExpenses() {
+  const filterMonth = document.querySelector("#filter-month").value.toLowerCase();
+  const filterYear = document.querySelector("#filter-year").value.toLowerCase();
+  const expenseItems = document.querySelectorAll(".expense-list li");
+
+  for (const expenseItem of expenseItems) {
+    const expenseDate = expenseItem.querySelector(".expense-date").innerHTML.toLowerCase();
+    if (filterMonth !== "all" && filterYear !== "all") {
+      if (expenseDate.includes(filterMonth) && expenseDate.includes(filterYear)) {
+        expenseItem.style.display = "block";
+      } else {
+        expenseItem.style.display = "none";
+      }
+    } else {
+      expenseItem.style.display = "block";
+    }
+  }
+
+  updateTotalExpenseAmount();
+}
+
+// Event listener for the filter form
+document.querySelector(".filter-form").addEventListener("submit", (event) => {
+  event.preventDefault();
   filterExpenses();
-  
-  
+});
+
+// Initialize the script
+updateTotalExpenseAmount();
+filterExpenses();
+
